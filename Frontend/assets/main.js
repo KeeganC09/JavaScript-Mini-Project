ce5124b0105a207710aa7db44460678e5643b25e
Keegan Colaco <keeganpaulcolaco@gmail.com>
2021-11-02 00:38:59 +0530

    made changes

--- Frontend/assets/main.js
+++ Frontend/assets/main.js
@@ -3,10 +3,12 @@ $(document).ready(function () {
     $("#next").click(function () {
         $("#part1").hide();
         $("#part2").show();
+        $("#part3").hide();
     })
     $("#previous").click(function () {
         $("#part2").hide();
         $("#part1").show();
+        $("#part3").show()
     })
 });
 
@@ -55,27 +57,26 @@ function addData() {
         .then(data => console.log(data))
         .catch(error => console.log("ERROR"))
 }
-async function getCovidApi()
-{
-    const jsonFormatData= await fetch("https://api.covid19api.com/summary");
+async function getCovidApi() {
+    const jsonFormatData = await fetch("https://api.covid19api.com/summary");
     const jsFormatData = await jsonFormatData.json();
-   
-    const TotalglobalCase = jsFormatData.Global.TotalConfirmed ;
-    console.log("Total Global Case : " +TotalglobalCase);
-    
-    const countryName= jsFormatData.Countries[0].Country ;
-    console.log("First Country Name : "+countryName);
+
+    const TotalglobalCase = jsFormatData.Global.TotalConfirmed;
+    console.log("Total Global Case : " + TotalglobalCase);
+
+    const countryName = jsFormatData.Countries[0].Country;
+    console.log("First Country Name : " + countryName);
     console.log(jsFormatData.Countries[76].Country);
 
-    document.getElementById("g1").innerHTML = jsFormatData.Global.TotalConfirmed ;
-    document.getElementById("g2").innerHTML = jsFormatData.Global.TotalDeaths ;
-    document.getElementById("g3").innerHTML = jsFormatData.Global.NewConfirmed ;
-    
-    
-    document.getElementById("i1").innerHTML = jsFormatData.Countries[76].TotalConfirmed ;
-    document.getElementById("i2").innerHTML = jsFormatData.Countries[76].TotalDeaths ;
-    document.getElementById("i3").innerHTML = jsFormatData.Countries[76].NewConfirmed ;
-    
-    
+    document.getElementById("g1").innerHTML = jsFormatData.Global.TotalConfirmed;
+    document.getElementById("g2").innerHTML = jsFormatData.Global.TotalDeaths;
+    document.getElementById("g3").innerHTML = jsFormatData.Global.NewConfirmed;
+
+
+    document.getElementById("i1").innerHTML = jsFormatData.Countries[76].TotalConfirmed;
+    document.getElementById("i2").innerHTML = jsFormatData.Countries[76].TotalDeaths;
+    document.getElementById("i3").innerHTML = jsFormatData.Countries[76].NewConfirmed;
+
+
 }
 getCovidApi();
